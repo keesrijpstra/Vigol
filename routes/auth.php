1c5372d53005dea1e9bdfa6ce93b4e8a33784fec
keesrijpstra <keesrijpstrat@gmail.com>
2025-04-12 14:01:07 +0200

    sldkjfksldf

--- routes/auth.php
+++ routes/auth.php
@@ -1,16 +1,17 @@
 <?php
 
-use App\Http\Controllers\Auth\VerifyEmailController;
-use App\Livewire\Auth\ConfirmPassword;
-use App\Livewire\Auth\ForgotPassword;
+use App\Livewire\Auth;
 use App\Livewire\Auth\Login;
 use App\Livewire\Auth\Register;
-use App\Livewire\Auth\ResetPassword;
 use App\Livewire\Auth\VerifyEmail;
+use App\Livewire\Auth\ResetPassword;
+use App\Livewire\Auth\ForgotPassword;
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Auth\ConfirmPassword;
+use App\Http\Controllers\Auth\VerifyEmailController;
 
 Route::middleware('guest')->group(function () {
-    Route::get('login', Login::class)->name('login');
+    Route::get('login', Auth::class)->name('login');
     Route::get('register', Register::class)->name('register');
     Route::get('forgot-password', ForgotPassword::class)->name('password.request');
     Route::get('reset-password/{token}', ResetPassword::class)->name('password.reset');
